13d71d5e600769f0210249628f415d437537ed28
Matheus Pires <91151628+MattLPires@users.noreply.github.com>
2022-03-15 15:15:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const restify = require('restify');
+
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root', 
+    password: '',
+    database: 'dbs'
+  }
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.listen(3030, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+server.get('/', function(req, res, next){
+
+  knex('rest').then((dados)=>{
+    res.send(dados);
+
+}, next);
+
+  
+
+  //return next();
+
+});
